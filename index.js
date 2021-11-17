5ed05917bd2cd0ea4c173f4ab72f1ece829ebdb4
Priscilla Okawa <priscilla.okawa@ibm.com>
2021-11-17 10:49:45 -0800

    deploy test

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send({ hey: "there" });
+  res.send({ just: "testing" });
 });
 
 const PORT = process.env.PORT || 3000;
