bbf86ac69bb95cac3b76cb33b3f3b0bee55839b7
Priscilla Okawa <priscilla.okawa@ibm.com>
2021-11-18 18:09:53 -0800

    dev/prod environments config

--- service/passport.js
+++ service/passport.js
@@ -21,6 +21,7 @@ passport.use(
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
       callbackURL: "/auth/google/callback",
+      proxy: true, //relative path telling googleStrategy to handle the case when req runs into any proxy (heroku proxy)
     },
     (accessToken, refreshToken, profile, done) => {
       User.findOne({ googleId: profile.id }).then((userFound) => {
