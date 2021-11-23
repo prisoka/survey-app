61a340f72d3e2fbcaf9f2dee7d23d18776ad1996
Priscilla Okawa <priscilla.okawa@ibm.com>
2021-11-23 10:16:51 -0800

    setting up materializeCSS

--- client/src/Components/Header.js
+++ client/src/Components/Header.js
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Header = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   return (
-    <div>
-      <h1>Survey APP</h1>
-      <a href="">Login with Google</a>
-    </div>
+    <nav>
+      <div className="nav-wrapper">
+        <a href="#" className="left brand-logo">
+          Survey APP
+        </a>
+        <ul id="nav-mobile" className="right">
+          {isLoggedIn ? (
+            <li>
+              <a href="">Logout</a>
+            </li>
+          ) : (
+            <li>
+              <a href="">Login with Google</a>
+            </li>
+          )}
+        </ul>
+      </div>
+    </nav>
   );
 };
 
